d0b8507917f9eb0fd539dcbab181679c01c31b2d
Josh Humphries <2035234+jhump@users.noreply.github.com>
2024-01-29 20:28:32 -0500

    work-around concurrency bug in okio.Pipe (https://github.com/square/okio/issues/1412)

--- okhttp/src/main/kotlin/com/connectrpc/okhttp/OkHttpStream.kt
+++ okhttp/src/main/kotlin/com/connectrpc/okhttp/OkHttpStream.kt
@@ -32,6 +32,7 @@ import okio.Buffer
 import okio.BufferedSink
 import okio.BufferedSource
 import okio.Pipe
+import okio.withLock
 import java.io.IOException
 import java.util.concurrent.CountDownLatch
 
@@ -182,14 +183,24 @@ internal class PipeRequestBody(
     private val pipe = Pipe(pipeMaxBufferSize)
 
     /**
+     * Lock used to provide extra synchronization so that fold, write, and
+     * close can work correctly in the face of concurrency.
+     *
+     * See https://github.com/square/okio/issues/1412
+     */
+    private val pipeLock = pipe.lock
+
+    /**
      * Latch that signals when the pipe's sink is closed.
      */
     private val closed = CountDownLatch(1)
 
     fun write(buffer: Buffer) {
         try {
-            pipe.sink.write(buffer, buffer.size)
-            pipe.sink.flush()
+            pipeLock.withLock {
+                pipe.sink.write(buffer, buffer.size)
+                pipe.sink.flush()
+            }
         } catch (e: Throwable) {
             close()
             throw e
@@ -199,7 +210,9 @@ internal class PipeRequestBody(
     override fun contentType() = contentType
 
     override fun writeTo(sink: BufferedSink) {
-        pipe.fold(sink)
+        pipeLock.withLock {
+            pipe.fold(sink)
+        }
         if (!duplex) {
             // For non-duplex request bodies, okhttp3
             // expects this method to return only when
@@ -214,7 +227,9 @@ internal class PipeRequestBody(
 
     fun close() {
         try {
-            pipe.sink.close()
+            pipeLock.withLock {
+                pipe.sink.close()
+            }
         } catch (_: Throwable) {
             // No-op
         } finally {
