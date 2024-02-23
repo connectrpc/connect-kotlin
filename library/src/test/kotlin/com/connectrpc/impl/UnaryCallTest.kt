a8b1e58bef81c8334be2ff744b9448404d7f5446
Joshua Humphries <2035234+jhump@users.noreply.github.com>
2024-02-23 14:46:09 -0500

    Simplify UnaryBlockingCall (#225)
    * It's now just an interface, and the implementation is in the impl
    package (just like the stream interfaces).
    * The implementation is simpler and thread-safe. No more mutator methods
    needed.
    * It no longer supports calling it more than once (which it _ostensibly_
    did before, according to doc comments). This was incorrectly implemented
    anyway since, once invoked a subsequent time, it became impossible to
    cancel earlier invocations (and was not thread-safe). It is much easier
    to disallow multiple invocations than to try to make it properly support
    them. It's also much easier to reason about IMO.

---
+++ library/src/test/kotlin/com/connectrpc/impl/UnaryCallTest.kt
@@ -0,0 +1,106 @@
+// Copyright 2022-2023 The Connect Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.connectrpc.impl
+
+import com.connectrpc.Code
+import com.connectrpc.ConnectException
+import com.connectrpc.ResponseMessage
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+
+class UnaryCallTest {
+    @Test
+    fun testExecute() {
+        val executor = Executors.newSingleThreadExecutor()
+        try {
+            val result = Object()
+            val call = UnaryCall<Any> { callback ->
+                executor.execute {
+                    callback.invoke(
+                        ResponseMessage.Success(
+                            result,
+                            headers = emptyMap(),
+                            trailers = emptyMap(),
+                        ),
+                    )
+                }
+                return@UnaryCall { }
+            }
+            val resp = call.execute()
+            assertThat(resp).isInstanceOf(ResponseMessage.Success::class.java)
+            val msg = resp.success { it.message }!!
+            assertThat(msg).isEqualTo(result)
+        } finally {
+            assertThat(executor.shutdownNow()).isEmpty()
+        }
+    }
+
+    @Test
+    fun testCancelAfterExecute() {
+        testCancel(false)
+    }
+
+    @Test
+    fun testCancelBeforeExecute() {
+        testCancel(true)
+    }
+
+    private fun testCancel(cancelFirst: Boolean) {
+        val executor = Executors.newFixedThreadPool(2)
+        try {
+            // Indicates when the async task has begun.
+            val taskRunning = CountDownLatch(1)
+            // Indicates when the async task has been canceled.
+            val taskCanceled = CountDownLatch(1)
+
+            val call = UnaryCall<Any> { callback ->
+                executor.execute {
+                    taskRunning.countDown()
+                    taskCanceled.await()
+                    callback.invoke(
+                        ResponseMessage.Failure(
+                            headers = emptyMap(),
+                            trailers = emptyMap(),
+                            cause = ConnectException(code = Code.CANCELED),
+                        ),
+                    )
+                }
+                return@UnaryCall {
+                    taskCanceled.countDown()
+                }
+            }
+            if (cancelFirst) {
+                // When we execute the task below, the call will observe
+                // that it has already been canceled and immediately
+                // cancel the just-started task.
+                call.cancel()
+            } else {
+                // This will cancel the task right after it has started running.
+                executor.execute {
+                    taskRunning.await()
+                    call.cancel()
+                }
+            }
+            val resp = call.execute()
+            assertThat(resp).isInstanceOf(ResponseMessage.Failure::class.java)
+            val connEx = resp.failure { it.cause }!!
+            assertThat(connEx.code).isEqualTo(Code.CANCELED)
+        } finally {
+            assertThat(executor.shutdownNow()).isEmpty()
+        }
+    }
+}
