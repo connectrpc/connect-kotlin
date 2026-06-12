73c36187af28d42cdb26a6c6c39091e8762bd082
Philip K. Warren <pkwarren@users.noreply.github.com>
2026-06-12 10:16:25 -0500

    Update compile/target sdk and fix AGP warnings
    Newer dependencies (like okhttp) require that we compile to a later SDK.
    Fix two AGP warnings for deprecated settings which will go away in the
    next release.
    
    Signed-off-by: Philip K. Warren <pkwarren@users.noreply.github.com>

--- examples/android/build.gradle.kts
+++ examples/android/build.gradle.kts
@@ -3,12 +3,12 @@ plugins {
 }
 
 android {
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.connectrpc.examples.android"
         minSdk = 28
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
